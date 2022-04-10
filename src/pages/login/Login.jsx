d96655409e3cf475268cc9ef4044ce4900b34de4
frozen17 <zamirbekovmirlan3@gmail.com>
2022-04-10 12:09:50 +0600

    remove auth in server.js

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-        const res = await axios.post(`https://hpaptilek-54116aip8-frozen17.vercel.app/api/login`, {
+        const res = await axios.post(`https://hpap-spare-pants.herokuapp.com/api/login`, {
           email,
           password,
       });
